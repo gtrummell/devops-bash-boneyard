7ea30be831202974fbad10e798d160ae3b09209c
Graham Trummell <gtrummell@splunk.com>
2014-06-04 08:36:44 -0700

    Finished databag crypter

--- bin/databag-crypter.sh
+++ bin/databag-crypter.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
-DATABAG=${1}
-ITEM=${2}
 HOME=${HOME}
 
-for SERVER in `knifeblock which`; do
+DATABAG=${1}
+ITEM=${2}
+SECRET="${HOME}/.chef/data_bag.secret"
+
+for VAR in ${DATABAG} ${ITEM} ${HOME}; do
+    if [ -z ${VAR} ]; then
+        echo "All command-line arguments are required! Missing ${VAR}"
+    fi
+done
+
+for SERVER in `knifeblock list`; do
     knifeblock use ${SERVER}
-    knife data bag from file ${DATABAG} ${ITEM}.json --secret-file ${HOME}/.chef/data-bag.secret
-    knife data bag show ${DATABAG} ${ITEM} -F json --secret-file ${HOME}/.chef/data-bag.secret > ${SERVER}-${ITEM}.json
+    knife _10.16.2_ data bag from file ${DATABAG} "`pwd`/${ITEM}.json" --secret-file ${SECRET}
+    knife _10.16.2_ data bag show ${DATABAG} ${ITEM} -F json > ${SERVER}-${ITEM}-encrypted.json
+    knife _10.16.2_ data bag show ${DATABAG} ${ITEM} -F json --secret-file ${SECRET} > ${SERVER}-${ITEM}-cleartext.json
 done
