7fac1398a34a35364b07df3be0e472bb2101a491
Graham Trummell <gtrummell@splunk.com>
2014-08-30 00:37:17 -0700

    Update gitsync.rb, move linekill into bin

--- bin/gitsync.rb
+++ bin/gitsync.rb
@@ -7,13 +7,15 @@ libs.each do |lib|
   gem_lib = lib.gsub(/-/, '/')
   begin
     gem gem_lib
-  rescue
+  rescue => err_parent
     begin
+      puts("Gem #{gem_lib} not present. Attempting " +
+        "install.\n#{err_parent}")
       system('gem install ' + lib)
       Gem.clear_paths
-    rescue
-      raise('Gem install failed. Permission to install system gems or RVM' +
-        'required')
+    rescue => err_child
+      raise(puts('Gem install failed. Permission to install ' +
+        "system gems or RVM required!\n#{err_parent}\n#{err_child}"))
     end
   end
 
@@ -29,14 +31,16 @@ class GitSync
       if Dir.exists?(File.expand_path(dir))
         @source_root = File.expand_path(dir)
       end
-    rescue
-      raise Exception 'Target must be a directory containing git' +
-        'repositories' + dir
+    rescue => err
+      _logger('warn', 'Target must be a directory containing git ' +
+        "repositories: #{dir}\n#{err}")
+      false
     end
   end
 
   # Get a list of all directories containing a .git configuration dir
   def _find_repos
+
     begin
       repos = []
       Dir.glob(File.join(@source_root, '**/.git')).each do |dir|
@@ -44,20 +48,39 @@ class GitSync
       end
       @repos = repos
 
-      raise Exception 'No repositories found in ' + @source_root if @repos
-      .empty?
-    rescue
-      raise Exception 'Unable to search ' + @source_root
+      _logger('warn', 'No repositories found in ' + @source_root) if
+          @repos.empty?
+    rescue => err
+      _logger('fatal', 'Unable to search ' + @source_root + "\n" + err)
+    end
+  end
+
+  # Configure a logger instance
+  def _logger(severity, message, options={})
+    err = options[:error]
+    log_msg = Logger.new(STDOUT)
+
+    case severity
+      when 'info'
+        log_msg.info(message)
+        log_msg.info(err) if err
+      when 'warn'
+        log_msg.warn(message)
+        log_msg.warn(err) if err
+      when 'fatal'
+        log_msg.fatal(message)
+      else
+        log_msg.fatal(message)
     end
   end
 
   # Open a directory as a Git repo.  Raise a warning if it's not a git repo.
   def _open_repo(dir)
     begin
-      puts "Opening repo #{dir}"
+      _logger('info', 'Opening repo ' + dir)
       Git.open(dir, :log => Logger.new(STDOUT))
-    rescue
-      puts 'Not a git repository: ' + dir
+    rescue => err
+      _logger('warn', "Not a git repository: #{dir}\n#{err}")
     end
   end
 
@@ -66,41 +89,47 @@ class GitSync
     begin
       repo.checkout(branch)
       repo.pull
-    rescue
-      puts 'Unable to pull from source: ' + branch.to_s
+    rescue => err
+      _logger('warn', "Unable to pull from source for #{repo
+      .dir}/#{branch}\n#{err}")
     end
   end
 
   # Sync all the repos found in @source_root, parallelize by repo with threads.
+  # Remember which branch the user had checked out and check it out for them
+  # again.
   def sync
-    _find_repos
-    @repos.each do |repo|
-      repo_thread = Thread.new do
-        raise Exception 'Failed to open ' + repo unless
-          (open_repo = _open_repo(repo))
-        user_branch = open_repo.branch
+    begin
+      _find_repos
+      @repos.each do |repo|
+        next unless (open_repo = _open_repo(repo))
+        user_branch = open_repo.current_branch
 
-        open_repo.branches.local.each do |branch|
-          _pull_repo(open_repo, branch)
+        repo_thread = Thread.new do
+          open_repo.branches.local.each do |branch|
+            _pull_repo(open_repo, branch)
+          end
         end
+        repo_thread.join
 
         open_repo.checkout(user_branch)
       end
-      repo_thread.abort_on_exception = true
-      repo_thread.join
+    rescue => err
+      _logger('fatal', "Fatal sync error!\n#{err}")
+      raise err
     end
   end
 end
 
+# Command line logic for all-in-one Ruby Script utility
 if ARGV.empty? || ARGV.include?('--help' || '-h')
-  exit('Usage: gitsync <dir> [dir dir dir...]')
+  exit(puts 'Usage: gitsync <dir> [dir dir dir...]')
 else
   ARGV.each do |dir|
     source_root_thread = Thread.new {
       git_dir = GitSync.new(dir)
       git_dir.sync
     }
-    source_root_thread.abort_on_exception = true
     source_root_thread.join
   end
 end
