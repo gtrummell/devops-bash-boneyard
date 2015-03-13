0fefbffaa0b3e4cf4275e6092ba5da8e63891f56
Graham Trummell <graham@bandpage.com>
2015-03-12 23:51:57 -0700

    Sanitized scripts for [somewhat] public consumption

--- bin/knife-ec2-server-delete.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-NODE_NAME=${1}
-
-KNIFE_EXEC=`which knife`
-INSTANCE_ID=`${KNIFE_EXEC} search node "name:${NODE_NAME}" -a ec2.instance_id -F json | jq -r '.rows[][][]'`
-
-echo "Deleting ${NODE_NAME} with instance id ${INSTANCE_ID}"
-${KNIFE_EXEC} ec2 server delete ${INSTANCE_ID} --node-name ${NODE_NAME} --purge
